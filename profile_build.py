d03d239510f5ed831b738e9240eb7b7b86a4e56e
Alexander Freas <akfreas@gmail.com>
2016-11-24 17:56:17 +0100

    Added summary for swift compile time

--- profile_build.py
+++ profile_build.py
@@ -4,6 +4,24 @@ import ipdb
 import pandas
 
 
+class FileCompileInfo:
+
+    def __init__(self, duration, filename):
+
+        self.duration = float(duration)
+        self.filename = filename
+    
+    @classmethod
+    def csv_columns(cls):
+        return ",".join(["duration", "filename"])
+
+    def csv_summary(self):
+
+        return ",".join([
+            str(self.duration),
+            self.filename
+        ])
+
 class TimeInfo:
 
     def __init__(self):
@@ -96,8 +114,52 @@ def summarize_output(filename):
     print "Total: {0}\n".format(pda["duration_in_seconds"].sum())
     print grouped["duration_in_seconds"].sum()
 
+def gather_build_times_from_file(build_output_filename, matching_data):
+
+
+    file_rx = re.compile("^[0-9]*:\s*([\d*\.]*)ms\s[\w*\/\.-]*\/(\w*\.swift).*")
+
+    build_output = open(build_output_filename, "r")
+
+    comp_times = []
+
+    for line in build_output.readlines():
+
+        match = file_rx.match(line)
+
+        if match:
+
+            time, filename = match.groups()
+
+            info = FileCompileInfo(time, filename)
+            comp_times.append(info)
+
+    matching_file = open(matching_data, "w")
+
+    matching_file.write(FileCompileInfo.csv_columns()+"\n")
+
+    for comp_info in comp_times:
+
+        matching_file.write(comp_info.csv_summary()+"\n")
+
+    matching_file.close()
+
+def summarize_build_times(matching_data):
+
+    pda = pandas.read_csv(matching_data)
+
+    grouped = pda.groupby("filename")
+    total_swift_compile = pda["duration"].sum()
+    a_sum = grouped["duration"].sum()
+    a_sum.sort("duration")
+    top_ten = a_sum[-10:]
+    print """\n\nTotal swift compile time: {0}\nTop ten culprits taking total of {1} ms: {2}""".format(total_swift_compile, top_ten.sum(), top_ten)
+
 if __name__ == '__main__':
-    main()
+    summary = "compile_summary.csv"
+    gather_build_times_from_file("build_output.txt", summary)
+    summarize_build_times(summary)
+    #main()
 
 
 
